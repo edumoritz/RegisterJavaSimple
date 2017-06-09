94ea2ac9ff1fee472a5a7b979fb460fd53237d8a
EMORITZ <EMORITZ@WS-LAB08_143.univel.univel.br>
2017-06-08 21:09:36 -0300

    Commit by isahan

--- project-jasper-maven/src/main/java/br/univel/login/TelaComLogin.java
+++ project-jasper-maven/src/main/java/br/univel/login/TelaComLogin.java
@@ -17,6 +17,9 @@ public class TelaComLogin extends JFrame {
 
 	private JPanel contentPane;
 
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
